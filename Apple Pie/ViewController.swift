ea0c8d237db7c3259b5d9ff28583eb656091398d
Rey Aponte <reynaldo.apontedavila@montgomerycollege.edu>
2022-06-13 16:44:04 -0400

    In the middle of Begin First Round

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -11,6 +11,12 @@ class ViewController: UIViewController {
     
     var listOfWords = ["buccaneer", "swift", "glorious", "incandescent", "bug", "program"]
     let incorrectMovesAllowed = 7
+    
+    /*
+     “After each round, the bottom label will display an updated count of the number of wins and losses. Create two variables to hold each of these values, and set the initial values ​ to 0”
+     */
+    var totalWins = 0
+    var totalLosses = 0
 
     
     @IBOutlet var letterButtons: [UIButton]!
@@ -21,6 +27,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        newRound()
+    }
+    /*
+     “What does it mean to start a new round? Each round begins with the selection of a new word, and resetting the number of moves the player can make to incorrectMovesAllowed.”
+     */
+    func newRound(){
+        
     }
     @IBAction func letterButtonPressed(_ sender: UIButton) {
         sender.isEnabled = false
