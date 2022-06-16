4998a4e8f24b563cec5df0c7e5fb6ce85f786245
Rey Aponte <reynaldo.apontedavila@montgomerycollege.edu>
2022-06-16 14:39:35 -0400

    Completed Part 3

--- Apple Pie/Game.swift
+++ Apple Pie/Game.swift
@@ -10,6 +10,13 @@ import Foundation
 struct Game {
     var word: String
     var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
     
     
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
 }
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -38,7 +38,7 @@ class ViewController: UIViewController {
     
     func newRound(){
         let newWord = listOfWords.removeFirst()
-        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
         updateUI()
     }
     
@@ -48,6 +48,11 @@ class ViewController: UIViewController {
     }
     @IBAction func letterButtonPressed(_ sender: UIButton) {
         sender.isEnabled = false
+        let letterString = sender.title(for: .normal)!
+        let letter = Character(letterString.lowercased())
+        currentGame.playerGuessed(letter: letter)
+        updateUI()
+        
     }
 
 }
