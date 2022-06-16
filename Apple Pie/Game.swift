90b56523cd58360d73f0e4b7e8abc14938a46a46
Rey Aponte <reynaldo.apontedavila@montgomerycollege.edu>
2022-06-16 14:53:23 -0400

    Completed Part 4

--- Apple Pie/Game.swift
+++ Apple Pie/Game.swift
@@ -11,6 +11,17 @@ struct Game {
     var word: String
     var incorrectMovesRemaining: Int
     var guessedLetters: [Character]
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord.append(String(letter))
+            } else {
+                guessedWord.append("_")
+            }
+        }
+        return guessedWord
+    }
     
     
     mutating func playerGuessed(letter: Character) {
